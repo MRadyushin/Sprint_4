68fdd7d001b804a538ddb800f7a9c34427120a00
Maksim Radyushin <m.radyushin@yandex.ru>
2022-09-28 16:43:09 +0300

    Создание класса AnnotationTest. Класс содержит открытие и закрытие браузера.

---
+++ src/test/java/tests/AnnotationTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+    import org.openqa.selenium.WebDriver;
+    import org.openqa.selenium.chrome.ChromeDriver;
+    import org.junit.After;
+    import org.junit.Before;
+    import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AnnotationTests {
+
+    private WebDriver driver;
+
+    @Before
+    public void startUp() {
+        // Инициализируем WebDriverManager в начале каждого теста
+        WebDriverManager.chromedriver().setup();
+        // Создаем драйвер под Google Chrome
+        driver = new ChromeDriver();
+        // Открываем страницу сайта в браузере
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @After
+    public void tearDown() {
+        // Закрываем браузер
+        driver.quit();
+    }
+
+}
