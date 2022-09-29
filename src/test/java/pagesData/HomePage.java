5516006c74400640211bcb84de90ee1171e04375
Maksim Radyushin <m.radyushin@yandex.ru>
2022-09-29 11:00:22 +0300

    Досоздание класса HomePage описывающий главную страницу

--- src/test/java/pagesData/HomePage.java
+++ src/test/java/pagesData/HomePage.java
@@ -11,6 +11,42 @@ public class HomePage {
     // Локатор кнопки Заказа в теле
     private final By homeButtonBody = By.xpath("//*[@id=\'root\']/div/div/div[4]/div[2]/div[5]/button");
 
+    private String reqText;
+    private WebDriver driver;
 
+    //Локатор вопроса
+    private By question;
+    // Текст локатора ответа
+    private By answerText;
+
+    public HomePage(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    // Поиск и открытие вопроса
+    public void openRequests(String reqText) {
+        setQuestion(reqText);
+        WebElement elementQuestion = driver.findElement(question);
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollTo(0," + elementQuestion.getLocation().y + ")");
+        elementQuestion.click();
+    }
+    //Поиск вопроса
+    public void setQuestion(String reqText) {
+        //Подставляем текст и находим xpath вопроса
+        this.question = By.xpath(String.format(".//*[text()='%s']", reqText));
+    }
+    // Получение текста ответа
+    public String getAnswerText(String reqText) {
+        setAnswerText(reqText);
+        return driver.findElement(answerText).getText();
+    }
+    //Поиск ответа
+    public void setAnswerText(String reqText) {
+
+        //Подставляем текст и находим xpath ответа
+        this.answerText = By.xpath(String.format("(.//*[text()='%s'])/following::p[1]", reqText));
+    }
 }
 
